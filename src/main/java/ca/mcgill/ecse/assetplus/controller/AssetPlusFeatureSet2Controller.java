425b0ca84ecf759f78a2ed2a0633e42eb8eb34a8
DavidMarji <davidmmarji@hotmail.com>
2023-10-19 10:51:24 -0400

    implemented deleteAssetType and cleaned up other code

--- src/main/java/ca/mcgill/ecse/assetplus/controller/AssetPlusFeatureSet2Controller.java
+++ src/main/java/ca/mcgill/ecse/assetplus/controller/AssetPlusFeatureSet2Controller.java
@@ -15,8 +15,11 @@ public class AssetPlusFeatureSet2Controller {
 		return AssetPlusFeatureSet2Controller.validateInputs(name, expectedLifeSpanInDays);
 	}
     AssetType assetType = new AssetType(name, expectedLifeSpanInDays, AssetPlusApplication.getAssetPlus());
-    boolean placeHolderBool = ((AssetPlusApplication.getAssetPlus()).addAssetType(assetType));// IMPORTANT NOTE im not sure if this actually modifies the list of asset types since the method is a boolean one, but I think it should
-    return "";
+    
+    if ((AssetPlusApplication.getAssetPlus()).addAssetType(assetType)){// IMPORTANT NOTE im not sure if this actually modifies the list of asset types since the method is a boolean one, but I think it should
+    	return "";
+    }
+    return "unknown error";
   }
 
 	/**
@@ -25,6 +28,7 @@ public class AssetPlusFeatureSet2Controller {
    */
 
   public static String updateAssetType(String oldName, String newName, int newExpectedLifeSpanInDays) {
+	  
 	if(AssetPlusFeatureSet2Controller.validateInputs(newName, newExpectedLifeSpanInDays).length()!=0) {
 		return AssetPlusFeatureSet2Controller.validateInputs(newName, newExpectedLifeSpanInDays);
 	}
@@ -32,7 +36,7 @@ public class AssetPlusFeatureSet2Controller {
 	if(oldName==null || oldName.length()==0) { //check if null first so that java doesn't give an error if null was passed in (.length() would give an error when called on null), then check if empty.
 		return "The name must not be empty"; 
 	}
-	else if(AssetType.getWithName(oldName)==null) { //check if old name exists in the system
+	else if(AssetType.hasWithName(oldName)) { //check if old name exists in the system
 		return "The asset type does not exist";
 	}
 	
@@ -48,7 +52,10 @@ public class AssetPlusFeatureSet2Controller {
    * @author David Marji.
    */
   public static void deleteAssetType(String name) {
-    throw new UnsupportedOperationException("Not Implemented!");
+	if(AssetType.hasWithName(name)) {
+		AssetPlusApplication.getAssetPlus().removeAssetType(AssetType.getWithName(name)); //removes from system
+	}
+   AssetType.getWithName(name).delete(); //deletes
   }
   
 
@@ -64,7 +71,7 @@ public class AssetPlusFeatureSet2Controller {
 	    else if(expectedLifeSpan <= 0){
 	      return "The expected life span must be greater than 0 days";
 	    }
-	    else if(AssetType.getWithName("name")!=null){
+	    else if(AssetType.hasWithName(name)){
 	      return "The asset type already exists";
 	    }
 	  return "";
