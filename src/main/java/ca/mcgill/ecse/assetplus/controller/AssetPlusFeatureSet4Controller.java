c855296bf88e1c51fcafae25f6ca583667fd1845
AndersWoodruff <113076762+AndersWoodruff@users.noreply.github.com>
2023-10-19 22:34:27 -0400

    Update AssetPlusFeatureSet4Controller.java

--- src/main/java/ca/mcgill/ecse/assetplus/controller/AssetPlusFeatureSet4Controller.java
+++ src/main/java/ca/mcgill/ecse/assetplus/controller/AssetPlusFeatureSet4Controller.java
@@ -4,11 +4,9 @@ import ca.mcgill.ecse.assetplus.application.AssetPlusApplication;
 import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
-import ca.mcgill.ecse.assetplus.application.AssetPlusApplication;
 import ca.mcgill.ecse.assetplus.model.*;
 
 public class AssetPlusFeatureSet4Controller{
-
   // assetNumber -1 means that no asset is specified
   public static String addMaintenanceTicket(int id, Date raisedOnDate, String description,
     String email, int assetNumber) {
@@ -16,39 +14,20 @@ public class AssetPlusFeatureSet4Controller{
 
     User user = User.getWithEmail(email);
 
-    List<User> userList = new ArrayList<User>(assetplus.getEmployees());
-    userList.addAll(assetplus.getGuests());
-    userList.add(assetplus.getManager());
-
-    for (User u: userList){
-      if (u.getEmail() == email){
-        user = u;
-        break;
-      }
-    }
-
     if (user == null){
       return "The ticket raiser does not exist";
-    } else if (description == ""){
+    } else if (description == "" || description == null){
       return "Ticket description cannot be empty";
     }
-
     
     MaintenanceTicket newticket = new MaintenanceTicket(id, raisedOnDate, description, assetplus, user);
 
-
     if (assetNumber != -1){
-      boolean assetFound = false;
-      for (SpecificAsset k: assetplus.getSpecificAssets()){
-        if (k.getAssetNumber() == assetNumber){
-          newticket.setAsset(k);
-          assetFound = true;
-          break;
-        }
-      }
-      if (!assetFound){
+      SpecificAsset asset = SpecificAsset.getWithAssetNumber(assetNumber);
+      if (asset == null){
         return "The asset does not exist";
       }
+      newticket.setAsset(asset);
     }
 
 
@@ -58,70 +37,37 @@ public class AssetPlusFeatureSet4Controller{
   // newAssetNumber -1 means that no asset is specified
   public static String updateMaintenanceTicket(int id, Date newRaisedOnDate, String newDescription,
       String newEmail, int newAssetNumber) {
-    AssetPlus assetplus = AssetPlusApplication.getAssetPlus();
-
-    MaintenanceTicket ticket = null;
-
-    boolean ticketFound = false;
-    for (MaintenanceTicket l: assetplus.getMaintenanceTickets()){
-      if (l.getId() == id){
-        ticket = l;
-        ticketFound = true;
-        break;
-      }
-    }
+    MaintenanceTicket ticket = MaintenanceTicket.getWithId(id);
     if (ticket == null){
       return "The ticket does not exist";
     }
 
     User user = User.getWithEmail(newEmail); // temp fix to avoid controller errors
-    List<User> userList = new ArrayList<User>(assetplus.getEmployees());
-    userList.addAll(assetplus.getGuests());
-    userList.add(assetplus.getManager());
-
-    for (User u: userList){
-      if (u.getEmail() == newEmail){
-        user = u;
-        break;
-      }
-    }
 
     if (user == null){
       return "The ticket raiser does not exist";
-    } else if (newDescription == ""){
+    } else if (newDescription == "" || newDescription == null){
       return "Ticket description cannot be empty";
+    }
+
+    if (newAssetNumber != -1){
+      SpecificAsset asset = SpecificAsset.getWithAssetNumber(newAssetNumber);
+      if (asset == null){
+        return "The asset does not exist";
+      }
+      ticket.setAsset(asset);
     }
 
     ticket.setRaisedOnDate(newRaisedOnDate);
     ticket.setDescription(newDescription);
     ticket.setTicketRaiser(user);
 
-    if (newAssetNumber != -1){
-      boolean assetFound = false;
-      for (SpecificAsset k: assetplus.getSpecificAssets()){
-        if (k.getAssetNumber() == newAssetNumber){
-          ticket.setAsset(k);
-          assetFound = true;
-          break;
-        }
-      }
-      if (!assetFound){
-        return "The asset does not exist";
-      }
-    }
-
     return "";
   }
 
   public static void deleteMaintenanceTicket(int id) {
-    AssetPlus assetplus = AssetPlusApplication.getAssetPlus();
-
-    for (MaintenanceTicket l: assetplus.getMaintenanceTickets()){
-      if (l.getId() == id){
-        l.delete();
-        break;
-      }
-    }
+    MaintenanceTicket ticket = MaintenanceTicket.getWithId(id);
+    ticket.delete();
   }
 
 }
