f6bbbc40fdfddc189e63296a07b3e93b29a886dd
Mohamed Abdelrahman <mohamed.abdelrahmanibrahim@mail.mcgill.ca>
2023-10-21 16:03:21 -0400

    fix controller 5

--- src/main/java/ca/mcgill/ecse/assetplus/controller/AssetPlusFeatureSet5Controller.java
+++ src/main/java/ca/mcgill/ecse/assetplus/controller/AssetPlusFeatureSet5Controller.java
@@ -10,23 +10,31 @@ public class AssetPlusFeatureSet5Controller {
     if (imageURL == null || imageURL.length() == 0){
       err="Image URL cannot be empty";
     }
-    else if ((imageURL.length() < 6 
-    || !imageURL.substring(0, 6).equals("http://"))
-    || (imageURL.length() < 7 
-    || !imageURL.substring(0, 7).equals("https://"))
-    )
+//    else if (((imageURL.length() > 6) && (!imageURL.substring(0, 7).equals("http://")))
+//            || ((imageURL.length() > 7) && (!imageURL.substring(0, 8).equals("https://"))))
+    else
     {
-      err="Image URL must start with http:// or https://";
-    }
-  
-    try {
-      MaintenanceTicket ticket= MaintenanceTicket.getWithId(ticketID);
-      TicketImage ticketImage= new TicketImage(imageURL, ticket);
-      if (!ticket.addTicketImage(ticketImage)){
-        err ="Image already exists for the ticket"; 
+      try {
+      if ((!imageURL.substring(0, 7).equals("http://"))
+        && (!imageURL.substring(0, 8).equals("https://")))
+      {
+        err="Image URL must start with http:// or https://";
       }
     } catch (Exception e) {
-      err="Ticket does not exist";
+        err="Image URL must start with http:// or https://";
+    }
+    }
+
+    if (err == "") {
+      try {
+        MaintenanceTicket ticket = MaintenanceTicket.getWithId(ticketID);
+        TicketImage ticketImage = new TicketImage(imageURL, ticket);
+        if (!ticket.addTicketImage(ticketImage)) {
+          err = "Image already exists for the ticket";
+        }
+      } catch (Exception e) {
+        err = "Ticket does not exist";
+      }
     }
     return err;
   }
