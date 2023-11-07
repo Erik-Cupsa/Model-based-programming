10d23f3b9eb4bd01a8b0a73591c941233ebd4da7
ltJustWorks <43526682+ltJustWorks@users.noreply.github.com>
2023-11-07 15:54:27 -0500

    add boilerplate for ticket work controller

---
+++ src/main/java/ca/mcgill/ecse/assetplus/controller/MaintenanceTicketWorkController.java
@@ -0,0 +1,25 @@
+package ca.mcgill.ecse.assetplus.controller;
+
+import ca.mcgill.ecse.assetplus.model.HotelStaff;
+
+public class MaintenanceTicketWorkController {
+  public static String assignStaffToTicket(/*  */) {
+    return "TODO";
+  }
+
+  public static String startWorkOnTicket(/*  */) {
+    return "TODO";
+  }
+
+  public static String completeWorkOnTicket(/*  */) {
+    return "TODO";
+  }
+
+  public static String approveWorkOnTicket(/*  */) {
+    return "TODO";
+  }
+
+  public static String disapproveWorkOnTicket(/*  */) {
+    return "TODO";
+  }
+}
