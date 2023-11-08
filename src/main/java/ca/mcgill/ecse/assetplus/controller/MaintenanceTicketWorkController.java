46464c356fca648697a8ab03840c5d4497eb52a4
ltJustWorks <43526682+ltJustWorks@users.noreply.github.com>
2023-11-08 11:24:50 -0500

    finish maintenanceticketwork controller methods

--- src/main/java/ca/mcgill/ecse/assetplus/controller/MaintenanceTicketWorkController.java
+++ src/main/java/ca/mcgill/ecse/assetplus/controller/MaintenanceTicketWorkController.java
@@ -8,7 +8,7 @@ import ca.mcgill.ecse.assetplus.model.MaintenanceTicket.Status;
 import ca.mcgill.ecse.assetplus.model.MaintenanceTicket.TimeEstimate;
 
 public class MaintenanceTicketWorkController {
-  public static String assignStaffToTicket(int ticketId, HotelStaff staff, PriorityLevel aPriority,TimeEstimate aTimeToResolve, Boolean requiresApproval) {
+  public static String assignStaffToTicket(int ticketId, String employeeEmail, TimeEstimate aTimeToResolve, PriorityLevel aPriority, Boolean requiresApproval) {
     // constraints (look at feature file)
     MaintenanceTicket ticket = MaintenanceTicket.getWithId(ticketId);
     if (ticket == null) {
@@ -26,38 +26,78 @@ public class MaintenanceTicketWorkController {
     if (status.equals(Status.Closed)) {
       return "Cannot assign a maintenance ticket which is closed.";
     }
-    if (status.equals(Status.Assigned)) {
+    if (status.equals(Status.InProgress)) {
       return "Cannot assign a maintenance ticket which is in progress.";
     }
    
+    HotelStaff staff = (HotelStaff) HotelStaff.getWithEmail(employeeEmail);
+    if (staff == null) {
+      return "Staff to assign does not exist.";
+    }
+
     ticket.assign(staff, aPriority, aTimeToResolve, requiresApproval);
     return "";
   }
 
-  public static String startWorkOnTicket(/*  */) {
-    return "TODO";
-  }
-
-  public static String completeWorkOnTicket(/*  */) {
-    return "TODO";
-  }
-
-  public static String approveWorkOnTicket(/*  */) {
-    return "TODO";
-  }
-
-  public static String disapproveWorkOnTicket(int ticketId, Date date, String reason) {
+  public static String startWorkOnTicket(int ticketId) {
     MaintenanceTicket ticket = MaintenanceTicket.getWithId(ticketId);
     if (ticket == null) {
       return "Maintenance ticket does not exist.";
     }
 
     Status status = ticket.getStatus();
-
     if (status.equals(Status.Open)) {
-      return "Cannot disapprove a maintenance ticket which is open. ";
+      return "Cannot start a maintenance ticket which is open.";
+    }
+    if (status.equals(Status.Resolved)) {
+      return "Cannot start a maintenance ticket which is resolved.";
+    }
+    if (status.equals(Status.Closed)) {
+      return "Cannot start a maintenance ticket which is closed.";
+    }
+    if (status.equals(Status.InProgress)) {
+      return "The maintenance ticket is already in progress.";
+    }
+
+    ticket.startWork();
+    return "";
+  }
+
+  public static String completeWorkOnTicket(int ticketId) {
+    MaintenanceTicket ticket = MaintenanceTicket.getWithId(ticketId);
+    if (ticket == null) {
+      return "Maintenance ticket does not exist.";
+    }
+
+    Status status = ticket.getStatus();
+    if (status.equals(Status.Open)) {
+      return "Cannot complete a maintenance ticket which is open.";
     }
     if (status.equals(Status.Assigned)) {
+      return "Cannot complete a maintenance ticket which is resolved.";
+    }
+    if (status.equals(Status.Closed)) {
+      return "The maintenance ticket is already closed.";
+    }
+    if (status.equals(Status.Resolved)) {
+      return "The maintenance ticket is already resolved.";
+    }
+
+    ticket.completeFix();
+    return "TODO";
+  }
+
+  public static String approveWorkOnTicket(int ticketId) {
+    MaintenanceTicket ticket = MaintenanceTicket.getWithId(ticketId);
+    if (ticket == null) {
+      return "Maintenance ticket does not exist.";
+    }
+
+    Status status = ticket.getStatus();
+    if (status.equals(Status.Open)) {
+      return "Cannot  a maintenance ticket which is open. ";
+    }
+    if (status.equals(Status.Resolved)) {
       return "Cannot disapprove a maintenance ticket which is resolved.";
     }
     if (status.equals(Status.Closed)) {
@@ -67,7 +107,32 @@ public class MaintenanceTicketWorkController {
       return "Cannot disapprove a maintenance ticket which is in progress.";
     }
 
-    ticket.rejectFix(date, reason);
+    ticket.acceptFix();
+    return "";
+  }
+
+  public static String disapproveWorkOnTicket(int ticketId, Date date, String description) {
+    MaintenanceTicket ticket = MaintenanceTicket.getWithId(ticketId);
+    if (ticket == null) {
+      return "Maintenance ticket does not exist.";
+    }
+
+    Status status = ticket.getStatus();
+
+    if (status.equals(Status.Open)) {
+      return "Cannot disapprove a maintenance ticket which is open.";
+    }
+    if (status.equals(Status.Assigned)) {
+      return "Cannot disapprove a maintenance ticket which is assigned.";
+    }
+    if (status.equals(Status.Closed)) {
+      return "Cannot disapprove a maintenance ticket which is closed.";
+    }
+    if (status.equals(Status.InProgress)) {
+      return "Cannot disapprove a maintenance ticket which is in progress.";
+    }
+
+    ticket.rejectFix(date, description);
     return "";
   }
 }
