20264db0f5ece0702c1fea7d634a3f64956566ce
ltJustWorks <43526682+ltJustWorks@users.noreply.github.com>
2023-11-27 13:09:07 -0500

    status page fxml

---
+++ src/main/java/ca/mcgill/ecse/assetplus/javafx/fxml/controllers/ViewStatusPageController.java
@@ -0,0 +1,22 @@
+package ca.mcgill.ecse.assetplus.javafx.fxml.controllers;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.scene.layout.StackPane;
+
+public class ViewStatusPageController{
+    @FXML
+    private Button addTicketButton;
+
+    @FXML
+    public void addTicketClicked (ActionEvent event) {
+    }
+
+}
