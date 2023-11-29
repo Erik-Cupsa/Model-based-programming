009d726eb57a16a7540d59dff26c79600ef1176d
ltJustWorks <43526682+ltJustWorks@users.noreply.github.com>
2023-11-29 13:28:54 -0500

    update EditTicketPageController

---
+++ src/main/java/ca/mcgill/ecse/assetplus/javafx/fxml/controllers/EditTicketPageController.java
@@ -0,0 +1,8 @@
+package ca.mcgill.ecse.assetplus.javafx.fxml.controllers;
+
+import ca.mcgill.ecse.assetplus.controller.TOMaintenanceTicket;
+
+public class EditTicketPageController {
+    public void initialize(TOMaintenanceTicket selectedTicket) {
+    }
+}
