ac4c9c3129fa72901720eb4082748893e5208f3d
Erik-Cupsa <86483911+Erik-Cupsa@users.noreply.github.com>
2023-11-28 19:30:20 -0500

    Add files via upload
    empty controller as placeholder

---
+++ src/main/java/ca/mcgill/ecse/assetplus/javafx/fxml/controllers/AddMaintenanceTicketController.java
@@ -0,0 +1,9 @@
+package ca.mcgill.ecse.assetplus.javafx.fxml.controllers;
+
+import ca.mcgill.ecse.assetplus.controller.TOMaintenanceTicket;
+import javafx.fxml.FXML;
+
+import java.util.List;
+
+public class AddMaintenanceTicketController {
+}
