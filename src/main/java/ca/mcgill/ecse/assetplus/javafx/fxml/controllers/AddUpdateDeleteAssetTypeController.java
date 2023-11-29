b7f9103600aeaddc87d13367663fa7492b2a8865
DavidMarji <davidmmarji@hotmail.com>
2023-11-29 09:30:28 -0500

    changed identity property and On Action and started the AssetType controller

---
+++ src/main/java/ca/mcgill/ecse/assetplus/javafx/fxml/controllers/AddUpdateDeleteAssetTypeController.java
@@ -0,0 +1,51 @@
+package ca.mcgill.ecse.assetplus.javafx.fxml.controllers;
+
+import ca.mcgill.ecse.assetplus.controller.AssetPlusFeatureSet2Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+
+public class AddUpdateDeleteAssetTypeController {
+    
+    @FXML
+    private TextField assetTypeNameTextField;
+
+    @FXML
+    private TextField assetTypeExpectedLifeSpanTextField;
+
+    @FXML
+    private Button addAssetTypeButton;
+
+    @FXML
+    private Button updateAssetTypeButton;
+
+    @FXML
+    private Button deleteAssetTypeButton;
+
+
+    @FXML
+    public void addAssetTypeClicked(ActionEvent event){
+
+    }
+
+    @FXML
+    public void updateAssetTypeClicked(ActionEvent event){
+
+    }
+    
+    @FXML
+    public void deleteAssetTypeClicked(ActionEvent event){
+
+    }
+
+
+}
