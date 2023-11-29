2005b131d59f3708dd39ed78cb02c60c6b50adb9
Erik-Cupsa <86483911+Erik-Cupsa@users.noreply.github.com>
2023-11-29 16:27:28 -0500

    Delete src/main/java/ca/mcgill/ecse/assetplus/javafx/fxml/controllers/EditTicketPageController.java

--- src/main/java/ca/mcgill/ecse/assetplus/javafx/fxml/controllers/EditTicketPageController.java
+++
@@ -1,8 +0,0 @@
-package ca.mcgill.ecse.assetplus.javafx.fxml.controllers;
-
-import ca.mcgill.ecse.assetplus.controller.TOMaintenanceTicket;
-
-public class EditTicketPageController {
-    public void initialize(TOMaintenanceTicket selectedTicket) {
-    }
-}
