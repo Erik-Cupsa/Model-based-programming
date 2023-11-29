95d9cc746692e9a952195d57dae4d90d8d991878
ltJustWorks <43526682+ltJustWorks@users.noreply.github.com>
2023-11-29 13:29:59 -0500

    update ViewStatusPage

--- src/main/java/ca/mcgill/ecse/assetplus/javafx/fxml/controllers/ViewStatusPageController.java
+++ src/main/java/ca/mcgill/ecse/assetplus/javafx/fxml/controllers/ViewStatusPageController.java
@@ -1,6 +1,10 @@
 package ca.mcgill.ecse.assetplus.javafx.fxml.controllers;
 
 import ca.mcgill.ecse.assetplus.controller.TOMaintenanceTicket;
+import javafx.application.Application;
+import javafx.beans.property.ReadOnlyObjectWrapper;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -8,13 +12,30 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class ViewStatusPageController{
     @FXML
-    private ListView<TOMaintenanceTicket> ticketsListView;
+    private TableView<TOMaintenanceTicket> ticketsTableView;
+    @FXML
+    private TableColumn<TOMaintenanceTicket, Integer> numberColumn;
+    @FXML
+    private TableColumn<TOMaintenanceTicket, String> issuerColumn;
+    @FXML
+    private TableColumn<TOMaintenanceTicket, String> statusColumn;
+    @FXML
+    private TableColumn<TOMaintenanceTicket, String> dateRaisedColumn;
     @FXML
     private Button addTicketButton;
     @FXML
@@ -28,23 +49,16 @@ public class ViewStatusPageController{
 
     @FXML
     public void initialize() {
-        ViewUtils.loadTickets(ticketsListView);
-    }
-
-    public void setTicketsListView(ListView<TOMaintenanceTicket> tickets){
-        this.ticketsListView = tickets;
-        initialize();
+        ViewUtils.loadTicketsIntoTableView(ticketsTableView, numberColumn, issuerColumn, statusColumn, dateRaisedColumn);
     }
 
     @FXML
     public void addTicketClicked(ActionEvent event) {
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../pages/AddMaintenanceTicket.fxml"));
-            System.out.println("add");
             Parent newRoot = fxmlLoader.load();
 
             AddMaintenanceTicketController addMaintenanceTicketController = fxmlLoader.getController();
-            addMaintenanceTicketController.initialize();
             // Access the current stage
             Stage currentStage = (Stage) addTicketButton.getScene().getWindow();
 
@@ -59,7 +73,42 @@ public class ViewStatusPageController{
 
     @FXML
     public void editTicketClicked(ActionEvent event) {
-        ViewUtils.editSelectedTicket(ticketsListView);
+        TOMaintenanceTicket selectedTicket = ticketsTableView.getSelectionModel().getSelectedItem();
+
+        if (selectedTicket != null) {
+            openEditTicketPage(selectedTicket);
+        } else {
+            System.out.println("No ticket selected.");
+        }
+    }
+
+    @FXML
+    public void deleteTicketClicked(ActionEvent event) {
+        return;
+    }
+
+    /**
+     * Opens editTicketPage and passes selected ticket to the page.
+     * @param selectedTicket Ticket selected in the ListView.
+     * @author Manuel Hanna
+     */
+    // Method to open the edit page and pass the selected ticket
+    public void openEditTicketPage(TOMaintenanceTicket selectedTicket) {
+        System.out.println("edit button clicked");
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("EditTicketPage.fxml"));
+            Parent root = loader.load();
+
+            EditTicketPageController editTicketPageController = loader.getController();
+            editTicketPageController.initialize(selectedTicket);
+
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.setTitle("Edit Ticket");
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 
